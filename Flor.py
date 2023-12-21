ec6dbb55431eb2c459ee564e634d30f4ad7ddf86
Pinguno123 <126709380+Pinguno123@users.noreply.github.com>
2023-12-21 17:40:23 -0600

    V2.0

--- Flor.py
+++ Flor.py
@@ -1,40 +1,60 @@
 import turtle
 
 # Configuración inicial
-turtle.speed(15)
 turtle.pensize(3)
 turtle.bgcolor('black')
+turtle.Screen().setup(width=1.0, height=1.0)
+
+# Función para dibujar un tallo y hoja
+def draw_stem_and_leave(t):
+    t.pensize(2)
+    t.speed(1)
+    t.setheading(88)    
+    t.forward(250)
+
+    # Dibujar hoja
+    t.color('green')
+    t.circle(80, 70)
+    t.left(110)
+    t.circle(80, 70)
 
 # Función para dibujar un pétalo
-def draw_petal():
-    turtle.circle(60, 70)
-    turtle.left(120)
-    turtle.circle(60, 70)
-    turtle.left(120)
+def draw_petal(t):
+    t.speed(15)
+    t.circle(120, 70)
+    t.left(120)
+    t.circle(120, 70)
+    t.left(120)
 
-# Función para dibujar un tallo
-def draw_stem():
-    turtle.color('green')
-    turtle.pensize(5)
-    turtle.right(90)
-    turtle.forward(200)
-    turtle.left(90)
+# Crear una copia de la tortuga principal
+t1 = turtle.Turtle()
+t1.color('white')
 
-# Centrar el dibujo
+# Mover la copia a una posición diferente
+t1.penup()
+t1.goto(-200, 0)
+t1.pendown()
+
+# Ejecución del dibujo con ambas tortugas
+for _ in range(10):
+    draw_petal(turtle)
+    draw_petal(t1)
+    turtle.left(36)
+
+# Mover ambas tortugas a la posición para el tallo y la hoja
 turtle.penup()
 turtle.goto(0, 0)
 turtle.pendown()
 
-# Dibujar las flores
-turtle.color('white')
-for _ in range(20):
-    draw_petal()
-    turtle.left(36)
+t1.penup()
+t1.goto(-200, 0)
+t1.pendown()
 
-turtle.penup()
+# Dibujar el tallo y la hoja con ambas tortugas al mismo tiempo
+draw_stem_and_leave(turtle)
+draw_stem_and_leave(t1)
 
-# Dibujar el tallo
-draw_stem()
-
+# Configuración de la ventana emergente
 turtle.hideturtle()
+t1.hideturtle()
 turtle.done()
--- Pruebas.py
+++ Pruebas.py
@@ -1,32 +1,56 @@
+#Importación de la libreria Turtle
 import turtle
 
 # Configuración inicial
-turtle.speed(15)
 turtle.pensize(3)
 turtle.bgcolor('black')
 
+#Pantalla completa
+turtle.Screen().setup(width=1.0, height=1.0)
+
+#Partes del dibujo
+def draw_stem():
+    turtle.pensize(2)
+    turtle.speed(1)
+    turtle.setheading(88)    
+    turtle.forward(250)
+
 def draw_petal():
-    turtle.circle(120, 70)  # Aumenté el radio del círculo
+    turtle.speed(3)
+    turtle.circle(120, 70)
     turtle.left(120)
     turtle.circle(120, 70)
     turtle.left(120)
 
-# Centrar el dibujo
+def draw_leave():
+    turtle.speed(1)
+    turtle.circle(80, 70)
+    turtle.left(110)
+    turtle.circle(80, 70)
+
+#Centrado del dibujo
 turtle.penup()
 turtle.goto(0, 0)
 turtle.pendown()
 
+#Ejecución del dibujo
 turtle.color('white')
 for _ in range(20):
     draw_petal()
     turtle.left(36)
 
-def draw_stem():
-    turtle.color('red')
-    turtle.goto(0, -20)
-    turtle.forward(100)
+turtle.penup()
+turtle.goto(15, -250)
+turtle.pendown()
 
+draw_stem()
 
+turtle.penup()
+turtle.goto(15, -200)
+turtle.pendown()
 
+draw_leave()
+
+#Configuración de la ventana emergente
 turtle.hideturtle()
 turtle.done()
